dae7e185fa176047a9a57d918b8bf808b32b4d9d
Luiz Felipe Bari <luizfelipebari@gmail.com>
2022-08-10 18:42:22 -0300

    Feature: Configured the theme from application

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
